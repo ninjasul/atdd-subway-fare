559574b5f62be8afe2e3f2755eae063d382cbbdc
dongyub.park <dongyub.park@42dot.ai>
2024-08-08 13:05:37 +0900

    test: add FareCalculatorTest

--- src/main/java/nextstep/subway/application/FareCalculator.java
+++ src/main/java/nextstep/subway/application/FareCalculator.java
@@ -1,31 +1,27 @@
 package nextstep.subway.application;
 
 public class FareCalculator {
-    private static final int BASE_FARE = 1250;
-    private static final int EXTRA_FARE_PER_5KM = 100;
-    private static final int EXTRA_FARE_PER_8KM = 100;
-    private static final int DISTANCE_THRESHOLD_10KM = 10;
-    private static final int DISTANCE_THRESHOLD_50KM = 50;
-    private static final int UNIT_DISTANCE_5KM = 5;
-    private static final int UNIT_DISTANCE_8KM = 8;
+    public static final int BASE_FARE = 1250;
+    public static final int EXTRA_FARE_PER_5KM = 100;
+    public static final int EXTRA_FARE_PER_8KM = 100;
+    public static final int DISTANCE_THRESHOLD_10KM = 10;
+    public static final int DISTANCE_THRESHOLD_50KM = 50;
+    public static final int UNIT_DISTANCE_5KM = 5;
+    public static final int UNIT_DISTANCE_8KM = 8;
 
     public static int calculateFare(int distance) {
         if (distance <= DISTANCE_THRESHOLD_10KM) {
             return BASE_FARE;
         }
 
-        int fare = BASE_FARE;
-        fare += calculateOverFare(distance - DISTANCE_THRESHOLD_10KM, DISTANCE_THRESHOLD_50KM - DISTANCE_THRESHOLD_10KM, EXTRA_FARE_PER_5KM, UNIT_DISTANCE_5KM);
-
-        if (distance > DISTANCE_THRESHOLD_50KM) {
-            fare += calculateOverFare(distance - DISTANCE_THRESHOLD_50KM, Integer.MAX_VALUE, EXTRA_FARE_PER_8KM, UNIT_DISTANCE_8KM);
+        if (distance <= DISTANCE_THRESHOLD_50KM) {
+            int extraFare = (int) Math.ceil((distance - DISTANCE_THRESHOLD_10KM) / (double) UNIT_DISTANCE_5KM) * EXTRA_FARE_PER_5KM;
+            return BASE_FARE + extraFare;
         }
 
-        return fare;
-    }
 
-    private static int calculateOverFare(int overDistance, int threshold, int farePerUnit, int unitDistance) {
-        int unit = overDistance > threshold ? (int) Math.ceil((overDistance - threshold) / (double) unitDistance) : (int) Math.ceil(overDistance / (double) unitDistance);
-        return unit * farePerUnit;
+        int maxExtraFare10kmTo50km = (int) Math.ceil((DISTANCE_THRESHOLD_50KM - DISTANCE_THRESHOLD_10KM) / (double) UNIT_DISTANCE_5KM) * EXTRA_FARE_PER_5KM;
+        int extraFare8km = (int) Math.ceil((distance - DISTANCE_THRESHOLD_50KM) / (double) UNIT_DISTANCE_8KM) * EXTRA_FARE_PER_8KM;
+        return BASE_FARE + maxExtraFare10kmTo50km + extraFare8km;
     }
 }
---
+++ src/test/java/nextstep/subway/application/FareCalculatorTest.java
@@ -0,0 +1,99 @@
+package nextstep.subway.application;
+
+import static nextstep.subway.application.FareCalculator.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class FareCalculatorTest {
+
+    @ParameterizedTest
+    @DisplayName("기본 요금 구간일 때 요금은 기본 요금이다")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void 기본_요금_구간일_때_요금은_기본_요금이다(int distance) {
+        // given & when
+        int fare = FareCalculator.calculateFare(distance);
+
+        // then
+        assertThat(fare).isEqualTo(BASE_FARE);
+    }
+
+    @ParameterizedTest
+    @DisplayName("10KM 초과 시 추가 요금이 1번 부과되는 구간일 때 요금은 기본 요금 + 추가 요금 1번이다")
+    @ValueSource(ints = {11, 12, 13, 14, 15})
+    void _10KM_초과_시_추가_요금이_1번_부과되는_구간일_때_요금은_기본_요금_추가_요금_1번이다(int distance) {
+        // given & when
+        int fare = FareCalculator.calculateFare(distance);
+
+        // then
+        assertThat(fare).isEqualTo(BASE_FARE + EXTRA_FARE_PER_5KM);
+    }
+
+    @ParameterizedTest
+    @DisplayName("10KM 초과 시 추가 요금이 2번 부과되는 구간일 때 요금은 기본 요금 + 추가 요금 2번이다")
+    @ValueSource(ints = {16, 17, 18, 19, 20})
+    void _10KM_초과_시_추가_요금이_2번_부과되는_구간일_때_요금은_기본_요금_추가_요금_2번이다(int distance) {
+        // given & when
+        int fare = FareCalculator.calculateFare(distance);
+
+        // then
+        assertThat(fare).isEqualTo(BASE_FARE + 2 * EXTRA_FARE_PER_5KM);
+    }
+
+    @ParameterizedTest
+    @DisplayName("10KM 초과 시 추가 요금이 3번 부과되는 구간일 때 요금은 기본 요금 + 추가 요금 3번이다")
+    @ValueSource(ints = {21, 22, 23, 24, 25})
+    void _10KM_초과_시_추가_요금이_3번_부과되는_구간일_때_요금은_기본_요금_추가_요금_3번이다(int distance) {
+        // given & when
+        int fare = FareCalculator.calculateFare(distance);
+
+        // then
+        assertThat(fare).isEqualTo(BASE_FARE + 3 * EXTRA_FARE_PER_5KM);
+    }
+
+    @ParameterizedTest
+    @DisplayName("10KM 초과 시 추가 요금이 8번 부과되는 구간일 때 요금은 기본 요금 + 추가 요금 8번이다")
+    @ValueSource(ints = {46, 47, 48, 49, 50})
+    void _10KM_초과_시_추가_요금이_9번_부과되는_구간일_때_요금은_기본_요금_추가_요금_8번이다(int distance) {
+        // given & when
+        int fare = FareCalculator.calculateFare(distance);
+
+        // then
+        assertThat(fare).isEqualTo(BASE_FARE + 8 * EXTRA_FARE_PER_5KM);
+    }
+
+    @ParameterizedTest
+    @DisplayName("50km 초과 구간에 추가 요금이 1번 부과되는 구간일 때 요금은 기본 요금 + 추가 요금 9번이다")
+    @ValueSource(ints = {51, 52, 53, 54, 55, 56, 57, 58})
+    void _50KM_초과_구간에_추가_요금이_1번_부과되는_구간일_때_요금은_기본_요금_추가_요금_9번_8KM_추가_요금_1번이다(int distance) {
+        // given & when
+        int fare = FareCalculator.calculateFare(distance);
+
+        // then
+        assertThat(fare).isEqualTo(BASE_FARE + 9 * EXTRA_FARE_PER_5KM);
+    }
+
+    @ParameterizedTest
+    @DisplayName("50km 초과 구간에 추가 요금이 2번 부과되는 구간일 때 요금은 기본 요금 + 추가 요금 9번 + 8KM 추가 요금 1번이다")
+    @ValueSource(ints = {59, 60, 61, 62, 63, 64, 65, 66})
+    void _50KM_초과_구간에_추가_요금이_2번_부과되는_구간일_때_요금은_기본_요금_추가_요금_9번_8KM_추가_요금_1번이다(int distance) {
+        // given & when
+        int fare = FareCalculator.calculateFare(distance);
+
+        // then
+        assertThat(fare).isEqualTo(BASE_FARE + 9 * EXTRA_FARE_PER_5KM + EXTRA_FARE_PER_8KM);
+    }
+
+    @ParameterizedTest
+    @DisplayName("50km 초과 구간에 추가 요금이 3번 부과되는 구간일 때 요금은 기본 요금 + 추가 요금 9번 + 8KM 추가 요금 2번이다")
+    @ValueSource(ints = {67, 68, 69, 70, 71, 72, 73, 74})
+    void _50KM_초과_구간에_추가_요금이_3번_부과되는_구간일_때_요금은_기본_요금_추가_요금_9번_8KM_추가_요금_2번이다(int distance) {
+        // given & when
+        int fare = FareCalculator.calculateFare(distance);
+
+        // then
+        assertThat(fare).isEqualTo(BASE_FARE + 9 * EXTRA_FARE_PER_5KM + 2 * EXTRA_FARE_PER_8KM);
+    }
+}
